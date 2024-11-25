c8f68b38ff6e237ead6e2f6b50b041b2f1cf3cba
Thami-debugger <thamithami816@gmail.com>
2024-11-25 11:35:15 +0200

    Updated website with interactive JavaScript and new design

---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask, render_template, send_from_directory
+import os
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/cv')
+def cv():
+    return send_from_directory(os.getcwd(), 'Velaphi_Nkosi_CV.pdf')
+
+if __name__ == "__main__":
+    app.run(debug=True)
